8e40e2857a7750de75275affd2a0d0f6c6aa2720
iamOgunyinka <ogunyinkajoshua@yahoo.com>
2018-01-26 16:18:54 +0100

    Updated the core features

--- recent_updates.py
+++ recent_updates.py
@@ -5,17 +5,23 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os, requests, redis, json, time
 from BeautifulSoup import BeautifulSoup
-from datetime import date, datetime, timedelta
-from models import TvSeries, TvEpisode, DownloadLink
+from datetime import date, timedelta
+from multiprocessing import Queue
+from models import TvSeries, TvEpisode, DownloadLink, TvSeason
 import threading
-
+import os
+import requests
+import redis
+import json
 
 cache_pass, port_number = os.environ.get('redis_pass'), int(os.environ.get('redis_port'))
 data_cache = redis.StrictRedis(password=cache_pass, port=port_number)
+visited_sites = {}
+unregistered_series = []
 
-def get_data_or_none(link, callback_on_failure=None):
+
+def get_page_data(link, callback_on_failure=None):
     try:
         rsp = requests.get(link)
     except Exception as e:
@@ -30,22 +36,21 @@ def get_data_or_none(link, callback_on_failure=None):
         return None
     return rsp
 
-visited_sites = {}
 
-def todays_date():
-    return date.today() # - timedelta(1)
+def today_date():
+    return date.today() - timedelta(1)
 
-def list_series(link):
+
+def list_all_series(link):
     if visited_sites.get(link) is not None:
         return None
     visited_sites[link] = True
-    network_response = get_data_or_none(link)
+    network_response = get_page_data(link)
     if network_response is None:
         return None
     parsed_data = BeautifulSoup(network_response.content)
     data_list = parsed_data.findAll('div', attrs={'class': 'data main'})
-    #~ today = date.today()
-    today = todays_date()
+    today = today_date()
     today_releases = []
     for data_item in data_list:
         bold = data_item.b.string + data_item.b.nextSibling.string
@@ -59,53 +64,57 @@ def list_series(link):
             release_date = release_date[:-1] + '/' + str(today.year)
         release_date = release_date.split('/')
         try:
-            is_todays_release = date(int(release_date[-1]), int(release_date[1]), int(release_date[0])) == today
-            if is_todays_release:
-                today_releases.append({ 'name': series_name, 'season': season_name, 'episode': episode_name })
+            is_released_today = date(int(release_date[-1]), int(release_date[1]), int(release_date[0])) == today
+            if is_released_today:
+                today_releases.append({'name': series_name, 'season': season_name, 'episode': episode_name})
         except ValueError:
             continue
     return today_releases
 
+
 db = SQLAlchemy()
 
+
 def initialize_app():
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get( 'DB_URL' )
-    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-    db.init_app(app)
+    application = Flask(__name__)
+    application.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL')
+    application.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+    db.init_app(application)
     
-    return app
+    return application
+
 
 app = initialize_app()
 
 
 @app.before_first_request
 def before_any_request():
+    # noinspection PyUnresolvedReferences
     db.configure_mappers()
     db.create_all()
 
 
-def list_link_series(link, post_data_callback, *callback_args, **callback_kw ):
+def list_series_from_link(link, post_data_callback, *callback_args, **callback_kw):
     if visited_sites.get(link) is not None:
         return
     visited_sites[link] = True
-    network_response = get_data_or_none(link)
+    network_response = get_page_data(link)
     if network_response is None:
         return
     parsed_data = BeautifulSoup(network_response.content)
     data_list = parsed_data.findAll('div', attrs={'class': 'data'})
 
     for data_item in data_list:
-        ahrefs = data_item.findAll('a')
-        for ahref in ahrefs:
-            post_data_callback(ahref, *callback_args, **callback_kw)
+        hrefs = data_item.findAll('a')
+        for href in hrefs:
+            post_data_callback(href, *callback_args, **callback_kw)
 
 
-def dl_link_extractor(ahref_object, *args, **kwargs):
-    download_link = ahref_object.attrs[0][1]
+def download_link_extractor(ah_ref_object, **kwargs):
+    download_link = ah_ref_object.attrs[0][1]
     if download_link == '#':
         return
-    download_type = ahref_object.string.strip()
+    download_type = ah_ref_object.string.strip()
     try:
         if kwargs['data'].get('download_links') is None:
             kwargs['data']['download_links'] = []
@@ -114,14 +123,15 @@ def dl_link_extractor(ahref_object, *args, **kwargs):
     except:
         print 'Error'
 
+
 def add_episode_dlinks(episode_link, current_episode, data):
-    list_link_series(episode_link, dl_link_extractor, episode=current_episode, data=data)
+    list_series_from_link(episode_link, download_link_extractor, episode=current_episode, data=data)
 
 
-def append_episode(ahref, *args, **kwargs):
+def add_episode_to_season(ah_ref, **kwargs):
     expected_episode_name = kwargs['episode'].strip()
-    episode_link = ahref.attrs[0][1]
-    episode_name = ahref.string.strip()
+    episode_link = ah_ref.attrs[0][1]
+    episode_name = ah_ref.string.strip()
     if expected_episode_name.lower() == episode_name.lower():
         try:
             new_episode = TvEpisode(title=episode_name, page_link=episode_link)
@@ -137,30 +147,19 @@ def find_save_episode(todays_release_info):
     with app.app_context():
         tv_series = db.session.query(TvSeries).filter_by(title=todays_release_info.get('name')).first()
         if tv_series is None:
+            print '{} not found'.format(todays_release_info.get('name'))
+            unregistered_series.append(todays_release_info)
             return None
         season_title = todays_release_info.get('season')
         episode_name = todays_release_info.get('episode')
         for season in tv_series.seasons:
             if season.title == season_title:
-                list_link_series(season.page_link, append_episode, season=season,episode=episode_name,
-                                 data_info=todays_release_info)
+                list_series_from_link(season.page_link, add_episode_to_season, season=season, episode=episode_name,
+                                      data_info=todays_release_info)
                 return
 
 
-def run_data_handler():
-    updates_url = os.environ.get('UPDATE_URL')
-    today = todays_date()
-    table_key = 'orn:releases-' + str(today)
-    if data_cache.hlen(table_key) > 0:
-        return
-    
-    if visited_sites.has_key(updates_url):
-        visited_sites.pop(updates_url)
-    today_releases = list_series(updates_url)
-
-    if today_releases is None or len(today_releases) == 0:
-        return
-    # noinspection PyTypeChecker
+def process_episodes_for(today_releases, table_key):
     for release in today_releases:
         key = release.get('name') + '@@' + release.get('episode')
         try:
@@ -173,8 +172,122 @@ def run_data_handler():
             data_cache.hset(table_key, key, value)
 
 
+def run_data_handler():
+    updates_url = os.environ.get('UPDATE_URL')
+    today = today_date()
+    table_key = 'orn:releases-' + str(today)
+    if data_cache.hlen(table_key) > 0:
+        print 'Nothing to do'
+        return
+    
+    if updates_url in visited_sites:
+        visited_sites.pop(updates_url)
+    today_releases = list_all_series(updates_url)
+
+    if today_releases is None or len(today_releases) == 0:
+        print 'No releases for today'
+        return
+    # noinspection PyTypeChecker
+    process_episodes_for(today_releases, table_key)
+
+
+def database_updater(database_handler, reader):
+    my_log_file = open('./log.txt', 'w')
+    result = json.loads(reader.get_buffer())
+    for tv_series in result:
+        my_log_file.write('Doing one TV series\n')
+        show_title = tv_series.get('name')
+        seasons = tv_series.get('seasons')
+        tv_series = TvSeries(title=show_title)
+        for season in seasons:
+            my_log_file.write('Doing one TV season\n')
+            season_name = season.get('title')
+            season_page_link = season.get('page_link')
+            episodes = season.get('episodes')
+            tv_season = TvSeason(title=season_name, page_link=season_page_link)
+            for episode in episodes:
+                my_log_file.write('Doing one TV episode\n')
+                episode_name = episode.get('title')
+                page_link = episode.get('page_link')
+                download_links = episode.get('dl_links')
+                tv_episode = TvEpisode(title=episode_name, page_link=page_link)
+                for dl_link in download_links:
+                    download_type = dl_link.get('type')
+                    download_link = dl_link.get('link')
+                    tv_episode.download_links.append(DownloadLink(download_type=download_type, link=download_link))
+                tv_season.episodes.append(tv_episode)
+                my_log_file.flush()
+            tv_series.seasons.append(tv_season)
+        with app.app_context():
+            try:
+                database_handler.session.add(tv_series)
+            except Exception as e:
+                database_handler.session.rollback()
+                print e
+
+    with app.app_context():
+        database_handler.session.commit()
+        my_log_file.write('Done\n')
+
+
+def fetch_tv_seasons(callback, arguments, number_of_threads):
+    thread_list = []
+    for i in range(number_of_threads):
+        new_thread = threading.Thread(target=callback, args=[arguments])
+        thread_list.append(new_thread)
+        new_thread.start()
+    for my_thread in thread_list:
+        if my_thread.is_alive():
+            my_thread.join()
+
+
+def find_register_new_series(series):
+    from scrapper import direct_page_links, main_page_scrapper, store_tv_series, list_link_series
+    from scrapper import visiting_links, list_tv_seasons, tv_series, JsonObjectIO
+
+    main_page_scrapper()  # extracts all TV series into `visiting_links`
+    for links in visiting_links:
+        list_link_series(links, store_tv_series)
+
+    all_unregistered_series = [series_name.get('name') for series_name in series]
+    print all_unregistered_series
+    new_series_list = Queue()
+    while direct_page_links.qsize() > 0:
+        series_object = direct_page_links.get()
+        tv_title, tv_page_link = (series_object[0], series_object[1])
+        if tv_title in all_unregistered_series:
+            new_series_list.put([tv_title, tv_page_link])
+
+    direct_page_links.close()
+    if new_series_list.empty():
+        return
+
+    fetch_tv_seasons(callback=list_tv_seasons, arguments=new_series_list, number_of_threads=3)
+    new_series_list.close()
+
+    json_io = JsonObjectIO()
+    s = []
+    for series in tv_series:
+        s.append(tv_series[series].to_object())
+    json_io.write(json.dumps(s, indent=2, separators=(',', ': ')))
+
+    thr = threading.Thread(target=database_updater, args=[db, json_io])
+    thr.start()
+    thr.join()
+
+
+
 if __name__ == '__main__':
     new_task = threading.Thread(target=run_data_handler, args=[])
     new_task.start()
-    if new_task.isAlive():
-        new_task.join()
+    new_task.join()
+    
+    print len(unregistered_series)
+    if len(unregistered_series) > 0:
+        # we'll find this series, scan the seasons and add each episodes
+        find_register_new_series(unregistered_series)
+        today = today_date()
+        table_key = 'orn:releases-' + str(today)
+        new_series = [ series for series in unregistered_series ]
+        print new_series == unregistered_series
+        process_episodes_for(new_series, table_key)
--- scrapper.py
+++ scrapper.py
@@ -1,93 +1,115 @@
-import requests, os, json
+import requests
+import os
+import json
 from BeautifulSoup import BeautifulSoup
 from threading import Thread
-
+from multiprocessing import Queue
+from Queue import Empty
 
 visited_sites = {}
 visiting_links = []
-all_series = []
-direct_page_links = {}
+direct_page_links = Queue()
 tv_series = {}
+NUM_THREADS = 30
 
 main_url = os.environ.get('MAIN_URL')
 
-class DlLink():
+
+class JsonObjectIO():
+    def __init__(self):
+        self.buffer = ''
+
+    def write(self, new_string):
+        self.buffer += new_string
+
+    def get_buffer(self):
+        return self.buffer
+
+    def __repr__(self):
+        return self.get_buffer()
+
+
+class DlLink:
     def __init__(self, download_type, download_link):
         self.download_type = download_type
         self.download_link = download_link
-    
+
     def __cmp__(self, y):
         return self.__eq__(y)
 
     def __hash__(self):
         return self.download_type.__hash__()
-    
+
     def __eq__(self, another_dl_link):
-        return self.download_type == another_dl_link.download_type # and self.download_link == another_dl_link.download_link
-        
+        return self.download_type == another_dl_link.download_type
+
     def to_object(self):
         return {'type': self.download_type, 'link': self.download_link}
 
-class ShowEpisode():
+
+class ShowEpisode:
     def __init__(self, page_link, opt_title=None):
         self.link = page_link
         self.title = opt_title
         self.download_links = []
-    
+
     def __hash__(self):
         return self.title.__hash__()
-        
-    def __cmp__(self,y):
+
+    def __cmp__(self, y):
         return self.__eq__(y)
-    
+
     def __eq__(self, another_episode):
         return self.title == another_episode.title
 
     def add_download_link(self, download_link):
         if download_link is not None:
-            if not download_link in frozenset(self.download_links):
+            if download_link not in frozenset(self.download_links):
                 self.download_links.append(download_link)
-                
+
     def to_object(self):
-        return {'title': self.title, 'page_link': self.link, 'dl_links': [dl.to_object() for dl in self.download_links] }
+        return {'title': self.title, 'page_link': self.link, 'dl_links': [dl.to_object() for dl in self.download_links]}
 
 
-class ShowSeason():
+class ShowSeason:
     def __add_episode__(self, episode):
         if not isinstance(episode, ShowEpisode):
             raise ValueError('{} is not a valid episode instance'.format(episode))
-        if not episode in frozenset(self.episodes):
+        if episode not in frozenset(self.episodes):
             self.episodes.append(episode)
 
     def __init__(self, title, page_link):
         self.title = title
         self.page_link = page_link
         self.episodes = []
-    
+
     def add_episode(self, episode, *episodes):
         self.__add_episode__(episode)
         for new_episode in episodes:
             self.__add_episode__(new_episode)
-    
-    def to_object(self):
-        return { 'title': self.title, 'page_link': self.page_link, 'episodes': [ep.to_object() for ep in self.episodes] }
 
-class ShowTitle():
+    def to_object(self):
+        return {'title': self.title, 'page_link': self.page_link,
+                'episodes': [ep.to_object() for ep in self.episodes]}
+
+
+class ShowTitle:
     def __init__(self, title):
         self.title = title
         self.seasons = []
 
-    def add_season(self,season):
+    def add_season(self, season):
         if isinstance(season, ShowSeason):
             self.seasons.append(season)
         else:
             raise ValueError('Invalid season')
-            
+
     def to_object(self):
         return {'name': self.title, 'seasons': [season.to_object() for season in self.seasons]}
-    
+
     def __repr__(self):
         return json.dumps(self.to_object())
+
 
 def get_data_or_return(link, callback_on_failure=None):
     try:
@@ -98,7 +120,7 @@ def get_data_or_return(link, callback_on_failure=None):
             callback_on_failure()
         return None
     if not rsp.ok:
-        print 'Invalid response gotten from main page, code: {}'.format(response.status_code)
+        print 'Invalid response gotten from main page, code: {}'.format(rsp.status_code)
         if callback_on_failure is not None:
             callback_on_failure()
             return None
@@ -123,19 +145,19 @@ def main_page_scrapper():
                     visiting_links.append(attribute[1])
 
 
-def list_link_series(link, post_data_callback, *callback_args, **callback_kw ):
+def list_link_series(link, post_data_callback, *callback_args, **callback_kw):
     if visited_sites.get(link) is not None:
         return
     visited_sites[link] = True
     network_response = get_data_or_return(link)
-    if network_response is None: 
+    if network_response is None:
         return
     parsed_data = BeautifulSoup(network_response.content)
     data_list = parsed_data.findAll('div', attrs={'class': 'data'})
     for data_item in data_list:
-        ahrefs = data_item.findAll('a')
-        for ahref in ahrefs:
-            post_data_callback(ahref, *callback_args, **callback_kw)
+        ah_refs = data_item.findAll('a')
+        for ah_ref in ah_refs:
+            post_data_callback(ah_ref, *callback_args, **callback_kw)
     next_pages_info = parsed_data.findAll('div', attrs={'class': 'pagination'})
     for page in next_pages_info:
         next_pages = page.findAll('a')
@@ -146,72 +168,80 @@ def list_link_series(link, post_data_callback, *callback_args, **callback_kw ):
                     list_link_series(page_attrib[1], post_data_callback, *callback_args, **callback_kw)
 
 
-def store_tv_series(ahref, *args, **kwargs):
-    tv_title = ahref.string.rstrip()
-    tv_page_link = ahref.attrs[0][1]
-    direct_page_links[tv_title] = tv_page_link
+def store_tv_series(ah_ref):
+    tv_title = ah_ref.string.rstrip()
+    tv_page_link = ah_ref.attrs[0][1]
+    direct_page_links.put([tv_title, tv_page_link])
 
 
-def dl_link_extractor(ahref_object, *args, **kwargs):
-    download_link = ahref_object.attrs[0][1]
+def dl_link_extractor(ah_ref, **kwargs):
+    download_link = ah_ref.attrs[0][1]
     if download_link == '#':
         return
-    download_type = ahref_object.string.rstrip()
+    download_type = ah_ref.string.rstrip()
     kwargs['episode'].add_download_link(DlLink(download_type, download_link))
 
 
-def add_episode_dlinks(episode_link, current_episode):
+def add_episode_download_links(episode_link, current_episode):
     list_link_series(episode_link, dl_link_extractor, episode=current_episode)
 
 
-def append_episode(ahref, *args, **kwargs):
+def append_episode(ah_ref, **kwargs):
     tv_title = kwargs.get('title')
     season_name = kwargs.get('season')
     current_series = tv_series.get(tv_title)
-    episode_link = ahref.attrs[0][1]
-    episode_name = ahref.string.rstrip()
+    episode_link = ah_ref.attrs[0][1]
+    episode_name = ah_ref.string.rstrip()
     for season in current_series.seasons:
         if season_name == season.title:
             new_episode = ShowEpisode(episode_link, episode_name)
-            add_episode_dlinks(episode_link, new_episode)
+            add_episode_download_links(episode_link, new_episode)
             season.add_episode(new_episode)
             return
     print 'We could not find the season for the movie'
 
 
-def append_season(ahref, *args, **kw):
+def append_season(ah_ref, **kw):
     tv_title = kw.get('title').lower()
     title = tv_series.get(tv_title)
     if title is None:
         title = ShowTitle(kw.get('title'))
-    seasons_name = ahref.string.rstrip()
-    seasons_link = ahref.attrs[0][1]
+    seasons_name = ah_ref.string.rstrip()
+    seasons_link = ah_ref.attrs[0][1]
     print 'Adding {} to {}'.format(seasons_name, tv_title)
     title.add_season(ShowSeason(seasons_name, seasons_link))
-    tv_series[tv_title]=title
+    tv_series[tv_title] = title
     list_link_series(seasons_link, append_episode, title=tv_title, season=seasons_name)
 
 
-def list_tv_seasons(tv_title, page_link):
-    list_link_series(page_link, append_season, title=tv_title)
+def list_tv_seasons(page_links):
+    try:
+        while True:
+            tv_object = page_links.get_nowait()
+            tv_title, page_link = (tv_object[0], tv_object[1])
+            list_link_series(page_link, append_season, title=tv_title)
+    except Empty as empty_exception:
+        print empty_exception.message
 
 
 if __name__ == '__main__':
     main_page_scrapper()
-    #~ list_link_series(visiting_links[0], store_tv_series)
-    #~ tv_key = direct_page_links.keys()[0]
-    #~ list_tv_seasons(tv_key, direct_page_links[tv_key])
-    thread_list = []
     for links in visiting_links:
         list_link_series(links, store_tv_series)
-    for tv_key in direct_page_links:
-        new_thread = Thread(target=list_tv_seasons, args=[tv_key, direct_page_links[tv_key]])
+
+    thread_list = []
+    for i in range(NUM_THREADS):
+        new_thread = Thread(target=list_tv_seasons, args=[direct_page_links])
         thread_list.append(new_thread)
         new_thread.start()
-        #~ list_tv_seasons(tv_key, direct_page_links[tv_key])
+
+    print 'Joining threads'
     for my_thread in thread_list:
         if my_thread.is_alive():
             my_thread.join()
+    direct_page_links.close()
+
+    print 'Writing to file'
     with open('./tv_shows.txt', 'w') as my_file:
         s = []
         for series in tv_series:
